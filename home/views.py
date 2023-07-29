c3c53a8b6f9e619cf2fd07f50b908aac9adfe3d7
ohi <mahdinislamohi@gmail.com>
2023-07-29 23:59:37 +0600

    Created Search Functionality

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from home.models import Contact
 from django.contrib import messages
+from blog.models import Post
 
 # Create your views here.
 def home(request):
@@ -25,3 +26,9 @@ def contact(request):
 def about(request):
     return render(request, 'home/about.html')
     
+
+def search(request):
+    query=request.GET['query']
+    allPosts= Post.objects.filter(title__icontains=query)
+    params={'allPosts': allPosts}
+    return render(request, 'home/search.html', params)
